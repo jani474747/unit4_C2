0af90430872328558aa4b22cab557b5e6601fe55
jani474747 <ms9347474786@gmail.com>
2022-03-15 10:04:19 +0530

    evaluation

---
+++ src/configs/db.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose")
+//const 
+module.exports=()=>{
+    return mongoose.connect(
+      "mongodb+srv://vadivelan1998:vadivelan1234@cluster0.i3gis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+}
+//module.exports=connect
